baa0f1dfd490a6299a556505e8cb2eac7f7b865e
NISHANT NIKHIL <nishantnikhil360@gmail.com>
2026-08-04 22:55:20 +0530

    Time: 0 ms (100%), Space: 9.9 MB (5.21%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        stringstream ss(s);
+        string word;
+        string lastWord;
+
+        while (ss >> word) {
+            lastWord = word;
+        }
+
+        return lastWord.size();
+    }
+};
