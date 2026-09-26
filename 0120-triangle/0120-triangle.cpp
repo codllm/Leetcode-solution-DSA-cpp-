fbc5c716882f3f0f7e0145928f59af6aa2f786cd
NISHANT NIKHIL <nishantnikhil360@gmail.com>
2026-09-26 14:41:30 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.41%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,24 +2,29 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
 
-        int n = triangle.size();
+        //now ill write the solution based on the
+        //bottom-up solution
+        // in this approcuh ill write to move to the dp[0][0] case so that i can just return dp[0][0]
 
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        // copy the last row
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
         for (int j = 0; j < n; j++)
         {
             dp[n - 1][j] = triangle[n - 1][j];
         }
-        // Fill from second last row upwards
-        for (int i = n - 2; i >= 0; i--)
+        //dp array mein triangle ka last row ko copy krdo
+
+        for(int i=n-2;i>=0;i--)
         {
-            for (int j = 0; j <= i; j++)
+            for(int j=0;j<=i;j++)
             {
-                dp[i][j] = triangle[i][j] +
-                           min(dp[i + 1][j], dp[i + 1][j + 1]);
+                int sameith = dp[i+1][j];
+                int diffith = dp[i+1][j+1];
+                dp[i][j] = triangle[i][j] +  min(sameith,diffith);
             }
         }
 
         return dp[0][0];
+        
     }
 };
